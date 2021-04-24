42c9d12ad400a20a30a1a0babd5ef48654ad823d
Dani Alcala <daniamcode@gmail.com>
2021-04-24 23:26:13 +0200

    treat module.exports as a function so we can consider dev, common and production modes in order to import different plugins into the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 // ["@babel/plugin-proposal-optional-chaining"] plugin is probably no longer needed, because it is included in @babel/preset-env
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+
 const javascriptRules = {
   test: /\.js$/,
   exclude: /node_modules/,
@@ -12,18 +14,40 @@ const javascriptRules = {
   },
 };
 
-module.exports = {
 // output is main.js by default, but we can change it. We can put a hash to make it unique if we need to cache it or whatever
-  output: {
-    filename: "main.[hash].js"
-  },
-  module: {
-    rules: [javascriptRules],
-  },
-  plugins: [
+// we can treat module.exports as an object or as a function
+// module.exports = {
+//   output: {
+//     filename: "main.[hash].js"
+//   },
+//   module: {
+//     rules: [javascriptRules],
+//   },
+//   plugins: [
+//       new HTMLWebpackPlugin({
+//           title: 'React Webpack Config',
+//           template: './src/index.html'
+//       })
+//   ]
+// };
+
+const developmentPlugins = []
+const productionPlugins = [
+  new CompressionPlugin()
+]
+
+module.exports = (env, {mode}) => ({
+    output: {
+      filename: "main.[hash].js"
+    },
+    module: {
+      rules: [javascriptRules],
+    },
+    plugins: [
+      ...(mode === 'production' ? productionPlugins : developmentPlugins),
       new HTMLWebpackPlugin({
-          title: 'React Webpack Config',
-          template: './src/index.html'
+        title: 'React Webpack Config',
+        template: './src/index.html'
       })
-  ]
-};
+    ].filter(Boolean)
+  });
